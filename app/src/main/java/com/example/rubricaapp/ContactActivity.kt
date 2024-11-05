219ace7c82ccea331db35e0a36780867c4c9b4df
20050014 <20050014@lab06.lminf.unipmn.it>
2024-11-05 10:56:55 +0100

    Added ContactActivity

--- app/src/main/java/com/example/rubricaapp/ContactActivity.kt
+++ app/src/main/java/com/example/rubricaapp/ContactActivity.kt
@@ -1,6 +1,7 @@
 package com.example.rubricaapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,10 @@ class ContactActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_contact)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val name = intent.getStringExtra("name")
+        val surname = intent.getStringExtra("surname")
+        val age = intent.getIntExtra("age", 0)
+        
     }
 }
--- app/src/main/java/com/example/rubricaapp/MainActivity.kt
+++ app/src/main/java/com/example/rubricaapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rubricaapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
@@ -21,15 +22,22 @@ class MainActivity : AppCompatActivity() {
         contacts["jane_smith"] = Contact(name = "Jane", surname = "Smith", age = 25)
 
         // var selection = findViewById<TextView>(R.id.selection)
-        var contactList = contacts.keys.toList()
+        var contactsList = contacts.keys.toList()
 
-        val contactListView = findViewById<ListView>(R.id.list)
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contactList)
-        contactListView.adapter = arrayAdapter
+        val contactsListView = findViewById<ListView>(R.id.list)
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contactsList)
+        contactsListView.adapter = arrayAdapter
 
-        contactListView.setOnItemClickListener { parent, _, position, _ ->
-            Log.d("contact_selected", contactList[position])
-            var selectedContact = contactList[position]
+        contactsListView.setOnItemClickListener { parent, _, position, _ ->
+            Log.d("contact_selected", contactsList[position])
+            val selectedContactName = contactsList[position]
+            val selectedContact = contacts[selectedContactName]
+
+            val it = Intent(applicationContext, ContactActivity::class.java)
+            it.putExtra("name", selectedContact?.name)
+            it.putExtra("surname", selectedContact?.surname)
+            it.putExtra("age", selectedContact?.age)
+            startActivity(it)
 
         }
     }
